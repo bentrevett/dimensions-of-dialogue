bc69f536180244e377d199bb71c9b13fb78ed939
bentrevett <bentrevett@gmail.com>
2020-06-27 01:35:10 +0100

    added some lower noise choices changed how seeding works

--- runner.py
+++ runner.py
@@ -3,6 +3,7 @@ import random
 
 while True:
 
+    seed = random.choice([1234, 0, 1])
     hid_dim = 128
     z_dim = 100
     v_dim = 1000 #random.choice([100, 1000])
@@ -15,20 +16,18 @@ while True:
     beta_2 = 0.999
     n_epochs = 10
     n_iters = random.choice([100, 250, 500])
-    noise_mult = random.choice([0, 1.0, 3.0, 5.0, 7.0, 9.0])
+    noise_mult = random.choice([0, 0.1, 0.25, 0.5, 1.0, 3.0, 5.0, 7.0, 9.0])
     noise_inc_min = random.choice([1.0, -100])
     if noise_inc_min == -100:
         noise_inc_fac = 1.0
     else:
         noise_inc_fac = random.choice([1.1, 1.25, 1.5, 2.0])
 
-    for seed in [0, 1, 1234, 1111]:
-
-        start = f'python main.py '
-        args_1 = f'--seed {seed} --hid_dim {hid_dim} --z_dim {z_dim} --v_dim {v_dim} '
-        args_2 = f'--image_channels {image_channels} --image_size {image_size} '
-        args_3 = f'--init_std {init_std} --batch_size {batch_size} --one_hot --lr {lr} --beta_1 {beta_1} '
-        args_4 = f'--beta_2 {beta_2} --n_epochs {n_epochs} --n_iters {n_iters} --noise_mult {noise_mult} '
-        args_5 = f'--noise_inc_min {noise_inc_min} --noise_inc_fac {noise_inc_fac}'
-        arg = start + args_1 + args_2 + args_3 + args_4 +args_5
-        subprocess.call(arg, shell = True)
+    start = f'python main.py '
+    args_1 = f'--seed {seed} --hid_dim {hid_dim} --z_dim {z_dim} --v_dim {v_dim} '
+    args_2 = f'--image_channels {image_channels} --image_size {image_size} '
+    args_3 = f'--init_std {init_std} --batch_size {batch_size} --one_hot --lr {lr} --beta_1 {beta_1} '
+    args_4 = f'--beta_2 {beta_2} --n_epochs {n_epochs} --n_iters {n_iters} --noise_mult {noise_mult} '
+    args_5 = f'--noise_inc_min {noise_inc_min} --noise_inc_fac {noise_inc_fac}'
+    arg = start + args_1 + args_2 + args_3 + args_4 +args_5
+    subprocess.call(arg, shell = True)
